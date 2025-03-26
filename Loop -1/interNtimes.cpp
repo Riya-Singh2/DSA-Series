cc82b84ab377ae3d196e5c5217d295d992d37e76
Riya-Singh2 <2riya.6j@gmail.com>
2025-03-26 21:59:08 +0530

    add file

---
+++ Loop -1/interNtimes.cpp
@@ -0,0 +1,13 @@
+// Print Riya 'n' times . Take 'n' as input from user.
+
+#include<iostream>
+using namespace std;
+int main(){
+  int n;
+  cout<<"enter the number:";
+  cin>>n;
+  for(int i=1; i<=n; i++){
+    cout<<"Riya Singh"<<endl;
+  }
+  
+}
---
+++ Loop-2/Loop2Assingment/ReverseNum.cpp
@@ -0,0 +1,1 @@
+//12.Reverse a given integer number
